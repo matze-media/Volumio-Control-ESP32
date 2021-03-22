935575dff015825b5dcd30a68da3b4b40c8a5743
matze-media <80007228+matze-media@users.noreply.github.com>
2021-03-22 19:06:36 +0100

    Update main.h
    add default parameter

--- src/main.h
+++ src/main.h
@@ -24,10 +24,14 @@ U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 #include <HTTPClient.h>
 
 // define wifi access
+const char* ssid = "SSID"; //gitignore
+const char* password = "WIFI PASSWORD"; //gitignore
 long RSSI=0;
 long lastReadWIFI=0;
 
 // define volumio IP
+const char* volumio_ip = "IP ADRESS of Volumio"; //gitignore
+int volumio_port = 80; //gitignore
 
 #define USE_SERIAL Serial
 WiFiMulti wifiMulti;
